541ce2921178541e5779c38aae9ed8aa54b57fce
Jose Carlos <JCFebres53@gmail.com>
2016-02-08 13:48:26 +0100

    PrimerComit
    Aqui

---
+++ Assets/Probando/CrearCasillas.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CrearCasillas : MonoBehaviour {
+
+	// Use this for initialization
+
+
+	public GameObject enemigo;
+	
+
+	void Start () {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ box_camera_rotate/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    // Use this for initialization
+
+    static GameManager instance;
+    public GameObject Enemigo;
+    public static GameManager Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        //Asigna esta instancia al campo instance
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);  //Garantiza que sólo haya una instancia de esta clase
+    }
+
+    private List<GameObject> enemigos=new List<GameObject>();
+
+    void Start () {
+        crearEnemigo();
+        
+    }
+	
+	// Update is called once per frame
+	public void crearEnemigo()
+    {
+        
+
+        GameObject aux=Instantiate(Enemigo, new Vector3(transform.position.x+generarplano(), transform.position.y+ generarplano(), transform.position.z-0.55f), Quaternion.identity)as GameObject;
+        
+        enemigos.Add(aux);
+    }
+
+    public float generarplano()
+    {
+        float a=0;  
+
+        if (Random.Range(1, 4) == 1)
+            a =-0.33f;
+        if (Random.Range(1, 4) == 2)
+            a =0f;
+        if (Random.Range(1, 4) == 3)
+            a =0.33f;
+        return a;
+    }
+}
